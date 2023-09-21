7a64307396ccc906badff8031304287829fedfd8
londhe-7083 <shubhamlondhe1583@gmail.com>
2023-09-21 17:12:20 +0530

    adding some code

--- application/views/user/product_inforamtion.php
+++ application/views/user/product_inforamtion.php
@@ -12,16 +12,26 @@ $images=explode('&&',$product_info[0]['product_image']);
                     { 
                     ?>
                     <div style="margin-top:10px;">
-                        <img src="<?=base_url()?>uploads/<?=$row?>" width="100%">
+                        <img src="<?=base_url()?>uploads/<?=$row?>" width="100%" onclick="openImage('<?=base_url()?>uploads/<?=$row?>')">
                     </div>
                     <?php
                         }
                     ?>
                 </div>
                 <div class="col-md-9">
-                <img src="<?=base_url()?>uploads/<?=$images[0]?>" width="100%">
+                <img id="full_image" src="<?=base_url()?>uploads/<?=$images[0]?>" width="100%">
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    var images = '<?=$product_info[0]['product_image']?>';
+    console.log(images.split('&&'));
+    setInaterval(function())
+    function openImage(path)
+    {
+        document.getElementById("full_image").src = path;
+    }
+</script>
