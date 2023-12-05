3b1472a3fe440891dd6eb3655c616484735ed465
londhe-7083 <shubhamlondhe1583@gmail.com>
2023-12-05 22:46:57 +0530

    adding footer

--- application/views/user/navbar.php
+++ application/views/user/navbar.php
@@ -9,6 +9,8 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="<?=base_url()?>admin_assets/css/style-2.css">
+  
 </head>
 
 <body>
